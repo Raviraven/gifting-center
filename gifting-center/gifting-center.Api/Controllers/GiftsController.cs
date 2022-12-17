be538db4101920e1758890643135a58599bb16f4
Michał Kruszewski <kruszewski2@gmail.com>
2022-12-17 23:22:04 +0100

    save commit

---
+++ gifting-center/gifting-center.Api/Controllers/GiftsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace gifting_center.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GiftsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult<List<>> GetGiftsForUser()
+        {
+            return null;
+        }
+    }
+}
