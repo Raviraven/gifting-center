3b64ee5697497d8563a2a27c5c6060839ad06140
Michał Kruszewski <kruszewski2@gmail.com>
2023-01-29 12:45:46 +0100

    feat: add dependency inj

--- gifting-center/gifting-center.Api/Program.cs
+++ gifting-center/gifting-center.Api/Program.cs
@@ -1,16 +1,32 @@
 ﻿using gifting_center.Data.Database;
+using gifting_center.Data.Repositories;
+using gifting_center.Data.Repositories.Interfaces;
+using gifting_center.Logic.Services;
+using gifting_center.Logic.Services.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddTransient<ICategoriesService, CategoriesService>();
+builder.Services.AddTransient<IGiftedUsersService, GiftedUserService>();
+builder.Services.AddTransient<IGiftsService, GiftsService>();
+
+builder.Services.AddTransient<ICategoriesRepository, CategoriesRepository>();
+builder.Services.AddTransient<IGiftedUsersRepository, GiftedUsersRepository>();
+builder.Services.AddTransient<IGiftsRepository, GiftsRepository>();
+
+builder.Services.AddDbContext<PostgresSqlContext>(opts => opts.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<PostgresSqlContext>(opts => opts.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
 
