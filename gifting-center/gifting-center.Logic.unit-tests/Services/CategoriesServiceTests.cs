24f3fa32ada4afe0dd04afe66d42bab83b94cf26
Michał Kruszewski <kruszewski2@gmail.com>
2023-02-09 20:32:15 +0100

    feat: add data unit tests project

---
+++ gifting-center/gifting-center.Logic.unit-tests/Services/CategoriesServiceTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace gifting_center.Logic.unit_tests.Services
+{
+	public class CategoriesServiceTests
+	{
+		public CategoriesServiceTests()
+		{
+		}
+
+		[Fact]
+		public async void should()
+		{
+			true.Should().Be(false);
+		}
+	}
+}
+
