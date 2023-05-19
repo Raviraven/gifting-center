ddbc2722f7b7999c492391148590cf4ffb2cc011
Michał Kruszewski <kruszewski2@gmail.com>
2023-05-19 22:39:56 +0200

    update copy-to-pi

--- .copy-to-pi.sh
+++ .copy-to-pi.sh
@@ -2,5 +2,5 @@ if [ -z $PI_ADDRESS ]; then
     echo "PI_ADDRESS local variable missing"
 else
     PI_ADDRESS=$PI_ADDRESS
-    eval "rsync -av --exclude-from=exclude-from-pi.txt ./ ${PI_ADDRESS}:/home/pi/news-scrapper"
+    eval "rsync -av --exclude-from=exclude-from-pi.txt ./ ${PI_ADDRESS}:/home/pi/gifting-center"
 fi
