4cf3e5451293a7df0b8585e64ed9799486a2a8d3
Michał Kruszewski <kruszewski2@gmail.com>
2023-09-24 12:40:05 +0200

    refactor: change modules import to use relative paths (#14)
    * relative paths for components till GiftForm.tsx
    
    * refactor: change modules import to use relative paths
    
    * save commit

--- ui-react/src/components/index.ts
+++ ui-react/src/components/index.ts
@@ -1,3 +1,24 @@
-import { GiftEdit } from './gift';
+import { GiftEdit, GiftAdd } from './gift';
+import { CategoriesList } from './categories-list';
+import { CategoryEdit, CategoryAdd } from './category';
+import { GiftedUserEdit, GiftedUserAdd } from './gifted-user';
+import { GiftedUserList } from './gifted-user-list';
+import { GiftsList } from './gifts-list';
+import { Layout, AdminLayout } from './layout';
 
-export { GiftEdit };
+import { TranslatedText } from './translated-text';
+
+export {
+  GiftEdit,
+  GiftAdd,
+  TranslatedText,
+  CategoryEdit,
+  GiftedUserEdit,
+  GiftsList,
+  Layout,
+  AdminLayout,
+  CategoryAdd,
+  GiftedUserAdd,
+  GiftedUserList,
+  CategoriesList,
+};
